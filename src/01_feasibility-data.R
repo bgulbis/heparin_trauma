824ab230b43c93aba8502ab12dd34e53989dd251
Brian Gulbis <brian.gulbis@gmail.com>
2018-09-11 10:36:57 -0500

    make data sets for feasibility eda

--- src/01_feasibility-data.R
+++ src/01_feasibility-data.R
@@ -11,3 +11,55 @@ dir_raw <- "data/raw"
 #       - Admit Date: 8/1/18 - 9/1/18
 #       - Medication (Generic): heparin
 #       - Nurse Unit (Med): HH STIC
+
+pts <- read_data(dir_raw, "patients", FALSE) %>%
+    as.patients()
+
+mbo_id <- concat_encounters(pts$millennium.id)
+
+# run MBO queries
+#   * Labs - Coags
+#   * Medications - Inpatient - Prompt
+#       - Medication (Generic): heparin
+
+drips <- read_data(dir_raw, "meds-inpt", FALSE) %>%
+    as.meds_inpt() %>%
+    filter(!is.na(event.tag))
+
+fixed <- drips %>%
+    filter(med.rate.units == "unit/hr") %>%
+    distinct(millennium.id)
+
+include <- drips %>%
+    distinct(millennium.id) %>%
+    anti_join(fixed, by = "millennium.id")
+
+labs <- read_data(dir_raw, "labs", FALSE) %>%
+    as.labs() %>%
+    tidy_data() %>%
+    semi_join(include, by = "millennium.id")
+
+data_heparin <- drips %>%
+    semi_join(include, by = "millennium.id") %>%
+    calc_runtime() %>%
+    summarize_data()
+
+data_ptt <- labs %>%
+    filter(lab == "ptt") %>%
+    left_join(
+        data_heparin[c("millennium.id", "start.datetime", "stop.datetime")],
+        by = "millennium.id"
+    ) %>%
+    filter(
+        lab.datetime >= start.datetime,
+        lab.datetime <= stop.datetime
+    )
+
+data_rates <- drips %>%
+    semi_join(include, by = "millennium.id") %>%
+    filter(!is.na(med.rate.units))
+
+data_patients <- pts %>%
+    semi_join(include, by = "millennium.id")
+
+dirr::save_rds("data/tidy", "data_")
