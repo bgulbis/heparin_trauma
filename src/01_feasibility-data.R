5fe41ba15de8c18a2a6916873b8acdc9e63c753d
Brian Gulbis <brian.gulbis@gmail.com>
2018-09-11 09:38:58 -0500

    mbo query details

--- src/01_feasibility-data.R
+++ src/01_feasibility-data.R
@@ -1,5 +1,13 @@
-#
+# feasibility data for QI project looking at heparin dosing in trauma patients
 
 library(tidyverse)
 library(edwr)
 
+dir_raw <- "data/raw"
+
+# run MBO query
+#   * Patients - by Medication (Generic) - Location
+#       - Facility (Curr): HH HERMANN
+#       - Admit Date: 8/1/18 - 9/1/18
+#       - Medication (Generic): heparin
+#       - Nurse Unit (Med): HH STIC
